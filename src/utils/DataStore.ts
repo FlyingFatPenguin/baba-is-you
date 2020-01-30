068aed69efde8c4668a87bf14478d64cb5b33e15
FlyingFatPenguin <justAUselessEmail@163.com>
2020-01-30 15:40:46 +0800

    feat: 加入了 Cookie 数据存储

---
+++ src/utils/DataStore.ts
@@ -0,0 +1,24 @@
+export function saveData(name: string, value: string): void {
+  const exdate = new Date();
+  const expiredays = 30;
+  exdate.setDate(exdate.getDate() + expiredays);
+  document.cookie = name + "=" + escape(value) +
+    ((expiredays == null) ? "" : ";expires=" + exdate.toUTCString())
+}
+
+export function getData(name: string): string {
+  if (document.cookie.length > 0) {
+    let c_start = document.cookie.indexOf(name + "=");
+    if (c_start !== -1) {
+      c_start = c_start + name.length + 1;
+      let c_end = document.cookie.indexOf(";", c_start);
+      if (c_end === -1) {
+        c_end = document.cookie.length;
+      }
+
+      return unescape(document.cookie.substring(c_start, c_end));
+    }
+  }
+
+  return "";
+}
