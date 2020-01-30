102928f82fa25174c397cd80e13c62e783370ab9
FlyingFatPenguin <justAUselessEmail@163.com>
2020-01-30 15:40:17 +0800

    feat: 使用弹性布局扩展了 DropDown 的适用性

--- src/components/BabaHelp.tsx
+++ src/components/BabaHelp.tsx
@@ -7,16 +7,18 @@ interface Props {
 
 export default class BabaHelp extends React.Component<Props> {
   render() {
-    return <DropDown iconName='help'>
+    return <DropDown iconName='help' style={{ top: '15px', left: '10px' }}>
       <table>
-        <tr><td>动作</td><td>键盘</td><td>手势</td></tr>
-        <tr><td>向左</td><td> ⬅ </td><td>向左滑动</td></tr>
-        <tr><td>向右</td><td> ➡ </td><td>向右滑动</td></tr>
-        <tr><td>向上</td><td> ⬆ </td><td>向上滑动</td></tr>
-        <tr><td>向下</td><td> ⬇ </td><td>向下滑动</td></tr>
-        {/* 备选方案 🔄 🔁 */}
-        <tr><td>撤销</td><td> U </td><td>↪逆时针画圆</td></tr>
-        <tr><td>重玩本关</td><td> R </td><td>↩顺时针画圆</td></tr>
+        <tbody>
+          <tr><td>动作</td><td>键盘</td><td>手势</td></tr>
+          <tr><td>向左</td><td> ⬅ </td><td>向左滑动</td></tr>
+          <tr><td>向右</td><td> ➡ </td><td>向右滑动</td></tr>
+          <tr><td>向上</td><td> ⬆ </td><td>向上滑动</td></tr>
+          <tr><td>向下</td><td> ⬇ </td><td>向下滑动</td></tr>
+          {/* 备选方案 🔄 🔁 */}
+          <tr><td>撤销</td><td> U </td><td>↪逆时针画圆</td></tr>
+          <tr><td>重玩本关</td><td> R </td><td>↩顺时针画圆</td></tr>
+        </tbody>
       </table>
     </DropDown>
   }
--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -3,13 +3,14 @@ import '../css/DropDown.css'
 
 interface Props {
   iconName: string
+  style?: React.CSSProperties
 }
 
 class DropDown extends React.Component<Props>{
   render() {
     const iconName = this.props.iconName
     const children = this.props.children
-    return <div className='drop-down'>
+    return <div className='drop-down' style={this.props.style}>
       <img src={require('../img/' + iconName + '.png')} alt="" />
       <div>
         {children}
