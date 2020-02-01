6593a41ec7fa1e937240e7f3b7dbd92aac619325
FlyingFatPenguin <justAUselessEmail@163.com>
2020-02-01 14:33:13 +0800

    feat: 加入了地图编辑器调整大小的功能

--- src/MapEditor/components/ControlPanel.tsx
+++ src/MapEditor/components/ControlPanel.tsx
@@ -5,13 +5,7 @@ import DropDown from './DropDown';
 export default class ControlPanel extends React.Component {
   render() {
     return <div className='control-panel'>
-      <DropDown iconName='size'>
-        <h1>hhh</h1>
-        <input type="range"/>
-      </DropDown>
-      <DropDown iconName='grid'></DropDown>
-      <DropDown iconName='add'></DropDown>
-      <DropDown iconName='export'></DropDown>
+      {this.props.children}
     </div>
   }
 }
--- src/page/MapEditor.tsx
+++ src/page/MapEditor.tsx
@@ -1,49 +1,60 @@
 import * as React from 'react';
 import BabaScene from '../components/BabaScene';
-import { GameMap, GameObjectInterface } from '../GameCore/interface/Interface';
 import Scene from '../GameCore/Model/Scene';
 import ControlPanel from '../MapEditor/components/ControlPanel';
+import { mapBuilder } from '../GameCore/data/MapHelper';
+import DropDown from '../MapEditor/components/DropDown';
 
 interface Props {
 
 }
 
-interface States {
-  gameMap: GameMap
-}
-
-// interface SparseMap {
-//   [y: number]: {
-//     [x: number]: GameObjectInterface[]
-//   }
+// interface States {
+//   sizeX: number
+//   sizeY: number
 // }
 
-// function buildMapfromSparse(sizeX: number, sizeY: number, sparseMap: SparseMap) {
-//   const gameMap = Array(sizeY).fill('').map(() => Array(sizeX).fill(undefined))
-//   for (let y in Object.keys(sparseMap)) {
-//     const line = sparseMap[y]
-//     for (let x in Object.keys(line)) {
-//       const value = line[x]
-//       if (parseInt(x) < sizeX && parseInt(y) < sizeY) {
-//         gameMap[y][x] = value
-//       }
+
+// export default class MapEditor extends React.Component<Props, States> {
+//   constructor(props: Props) {
+//     super(props)
+//     this.state = {
+//       sizeX: 10,
+//       sizeY: 10,
 //     }
 //   }
-//   return gameMap
+//   render() {
+//     const { sizeX, sizeY } = this.state
+//     const gameMap = mapBuilder(sizeX, sizeY).build()
+//     return <div>
+//       <BabaScene scene={new Scene(gameMap)} showPos></BabaScene>
+//       <ControlPanel></ControlPanel>
+//     </div>
+//   }
 // }
 
-export default class MapEditor extends React.Component<Props, States> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      gameMap: []
+export default function MapEditor(props: Props) {
+  const [sizeX, setSizeX] = React.useState(10)
+  const [sizeY, setSizeY] = React.useState(10)
+  const gameMap = mapBuilder(sizeX, sizeY).build()
+
+  function handleClick(callback: (newValue: number) => void) {
+    return function (ev: React.ChangeEvent<HTMLInputElement>) {
+      callback(parseInt(ev.currentTarget.value, 10))
     }
   }
-  render() {
-    const gameMap = this.state.gameMap
-    return <div>
-      <BabaScene scene={new Scene(gameMap)}></BabaScene>
-      <ControlPanel></ControlPanel>
-    </div>
-  }
+
+  return <div>
+    <BabaScene showPos scene={new Scene(gameMap)}></BabaScene>
+    <ControlPanel>
+      <DropDown iconName='size'>
+        <h1>hhh</h1>
+        <input type="range" max='30' min='6' step='2' value={sizeX} onChange={handleClick(setSizeX)} />
+        <input type="range" max='30' min='6' step='2' value={sizeY} onChange={handleClick(setSizeY)} />
+      </DropDown>
+      <DropDown iconName='grid'></DropDown>
+      <DropDown iconName='add'></DropDown>
+      <DropDown iconName='export'></DropDown>
+    </ControlPanel>
+  </div>
 }
