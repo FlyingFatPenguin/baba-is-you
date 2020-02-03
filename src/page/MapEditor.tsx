33b6fac6222f6e90066579e6e700db4d371b7af3
FlyingFatPenguin <justAUselessEmail@163.com>
2020-02-03 19:30:31 +0800

    feat: 加入了绘图工具栏

--- src/page/MapEditor.tsx
+++ src/page/MapEditor.tsx
@@ -10,6 +10,7 @@ import BabaIsYou from '../components/BabaIsYou';
 import copy from 'copy-to-clipboard'
 import { downloadFile } from '../utils/FileSystem';
 import GridEditor from '../MapEditor/components/GridEditor';
+import Icon from '../MapEditor/components/Icon';
 
 interface Props {
 
@@ -34,13 +35,29 @@ interface SparseMap {
   mapItems: MapItem[]
 }
 
+enum EditMode {
+  point, // 选择
+  pipette, // 提色
+  pen, // 画笔
+  eraser, // 橡皮
+}
+
 export default function MapEditor(props: Props) {
+  // 是否处于编辑状态
   const [isEdit, setIsEdit] = React.useState(true)
+  // 地图属性
   const [sizeX, setSizeX] = React.useState(10)
   const [sizeY, setSizeY] = React.useState(10)
   const [mapItems, setMapItems] = React.useState([] as MapItem[])
+  // 当前编辑位置
   const [currentPos, setCurrentPos] = React.useState(undefined as undefined | { x: number, y: number })
+  // 当前文件名
   const [name, setName] = React.useState('BabaMap')
+  // 编辑模式
+  const [editMode, setEditMode] = React.useState(EditMode.point)
+  // 画笔写出的参照
+  const [sourceGrid, setSourceGrid] = React.useState([] as GameObjectInterface[])
+
   const gameMap = mapItems.reduce((p, v) =>
     p.setPos(v.pos, () => v.gridData),
     mapBuilder(sizeX, sizeY))
@@ -51,11 +68,25 @@ export default function MapEditor(props: Props) {
       callback(parseInt(ev.currentTarget.value, 10))
     }
   }
-  async function editGrid(x: number, y: number) {
+  function handleGridClick(x: number, y: number) {
     if (currentPos) {
       setCurrentPos(undefined)
-    } else {
-      setCurrentPos({ x, y })
+      return
+    }
+    switch (editMode) {
+      case EditMode.point:
+        setCurrentPos({ x, y })
+        return
+      case EditMode.pen:
+        setGridData({ x, y }, sourceGrid)
+        return
+      case EditMode.pipette:
+        setSourceGrid(getGridData({ x, y }))
+        setEditMode(EditMode.pen)
+        return
+      case EditMode.eraser:
+        setGridData({ x, y }, [])
+        return
     }
   }
 
@@ -70,7 +101,7 @@ export default function MapEditor(props: Props) {
   }
   function getGridData(pos: Pos) {
     const result = mapItems.find(v => v.pos.x === pos.x && v.pos.y === pos.y)
-    return result && result.gridData
+    return result && result.gridData || []
   }
 
   function handleConfirm(objList: GameObjectInterface[]) {
@@ -124,14 +155,35 @@ export default function MapEditor(props: Props) {
     setName(ev.currentTarget.value)
   }
 
+  // ********* 模式管理 *********
+  function getIconName(mode: EditMode): string {
+    switch (mode) {
+      case EditMode.point:
+        return 'point'
+      case EditMode.pipette:
+        return 'pipette'
+      case EditMode.pen:
+        return 'pen'
+      case EditMode.eraser:
+        return 'eraser'
+    }
+  }
+
+  function changeMode(newMode: EditMode) {
+    setEditMode(newMode)
+  }
+
 
   return <div>
-    {isEdit &&
+    {
+      isEdit &&
+      editMode === EditMode.point &&
       currentPos &&
       <GridEditor onConfirm={handleConfirm} onCancel={handleCancel} startGrid={getGridData(currentPos) || []}></GridEditor>}
-    {isEdit ?
-      <BabaScene showPos scene={new Scene(gameMap)} onClick={editGrid}></BabaScene> :
-      <BabaIsYou startGameMap={gameMap} onWin={console.log}></BabaIsYou>
+    {
+      isEdit ?
+        <BabaScene showPos scene={new Scene(gameMap)} onClick={handleGridClick}></BabaScene> :
+        <BabaIsYou startGameMap={gameMap} onWin={console.log}></BabaIsYou>
     }
     <ControlPanel>
       <DropDown iconName='size'>
@@ -141,7 +193,12 @@ export default function MapEditor(props: Props) {
         高度: {sizeY}
         <input type="range" max='30' min='6' step='2' value={sizeY} onChange={changeMapSize(setSizeY)} />
       </DropDown>
-      {/* <DropDown iconName='grid'></DropDown> */}
+      <DropDown iconName={getIconName(editMode)}>
+        <Icon iconName='point' onClick={() => changeMode(EditMode.point)}></Icon>
+        <Icon iconName='pipette' onClick={() => changeMode(EditMode.pipette)}></Icon>
+        <Icon iconName='pen' onClick={() => changeMode(EditMode.pen)}></Icon>
+        <Icon iconName='eraser' onClick={() => changeMode(EditMode.eraser)}></Icon>
+      </DropDown>
       <DropDown iconName={isEdit ? 'play' : 'stop'} onClick={() => setIsEdit(!isEdit)}></DropDown>
       <DropDown iconName='import'>
         <h1>选择导入文件</h1>
